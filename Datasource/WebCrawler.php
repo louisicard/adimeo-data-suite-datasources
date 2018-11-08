b1f69c73289f7a4233634318643d2486715afad9
Louis Sicard <lsicard@core-techs.fr>
2018-11-08 09:44:39 +0100

    Web crawler

---
+++ Datasource/WebCrawler.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace AdimeoDataSuite\Datasource;
+
+use AdimeoDataSuite\Model\Datasource;
+
+class WebCrawler extends Datasource
+{
+
+  function getOutputFields()
+  {
+    return array(
+      'title',
+      'html',
+      'url',
+    );
+  }
+
+  function getSettingFields()
+  {
+    return array();
+  }
+
+  function getExecutionArgumentFields()
+  {
+    return array();
+  }
+
+
+  function execute($args)
+  {
+    //Nothing to do because it is based on call back
+  }
+
+  public function handleDataFromCallback($document){
+    $this->index($document);
+  }
+
+  function getDisplayName()
+  {
+    return 'Web crawler';
+  }
+
+}
